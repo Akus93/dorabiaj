f11ca315f0104eab974134c5e2f6c2b5e19cdd86
Michał Czupryniak <michal.czupryniak@gmail.com>
2016-04-07 18:25:12 +0200

    Update client project

--- client/src/components/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'app',
-    moduleId: module.id,
-    templateUrl: './app.component.html',
-    styleUrls : ['./app.component.css'],
-})
-export class AppComponent { }
