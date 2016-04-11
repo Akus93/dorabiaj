2722f931ee3770132ffba3076c1136d6bd1d9f35
Akus93 <dawid.rdzanek@gmail.com>
2016-04-11 12:40:17 +0200

    Rozbudowa Form - max/min_length, save

--- server/dorabiaj/forms.py
+++ server/dorabiaj/forms.py
@@ -1,9 +1,28 @@
+from .models import User
+
+
 class Form:
-    def __init__(self, fields):
-        self.fields = fields
+    def __init__(self, data):
+        try:
+            self.fields
+        except AttributeError:
+            self.fields = None
+        try:
+            self.required
+        except AttributeError:
+            self.required = []
+        try:
+            self.max_length
+        except AttributeError:
+            self.max_length = {}
+        try:
+            self.min_length
+        except AttributeError:
+            self.min_length = {}
+        self.data = data
         self.error = {}
         self.cleaned_data = {}
-        for key, value in self.fields.items():
+        for key, value in self.data.items():
             self.cleaned_data[key] = self.clean(value)
         self.check_all()
 
@@ -24,13 +43,56 @@ class Form:
                 except KeyError:
                     self.error[key] = 'Uzupełnij pole'
 
+    def check_max_length(self):
+            if self.max_length:
+                for key, value in self.max_length.items():
+                    if not self.error.get(key):
+                        if len(self.cleaned_data[key]) > value:
+                            try:
+                                self.error[key] = self.error_message[key]['max_length']
+                            except KeyError:
+                                self.error[key] = 'Pole posiada za duza liczbe znaków. Maksymalnie {}.'.format(value)
+
+    def check_min_length(self):
+        if self.min_length:
+            for key, value in self.min_length.items():
+                if not self.error.get(key):
+                    if len(self.cleaned_data[key]) < value:
+                        try:
+                            self.error[key] = self.error_message[key]['min_length']
+                        except KeyError:
+                            self.error[key] = 'Pole posiada za małą liczbe znaków. Minimalnie {}.'.format(value)
+
     def check_all(self):
         self.check_required()
+        self.check_max_length()
+        self.check_min_length()
+
+    def get_errors(self):
+        return self.error
+
+    def data_to_save(self):
+        if self.fields:
+            result = {}
+            for key, value in self.cleaned_data.items():
+                if key in self.fields:
+                    result[key] = value
+            return result
+        else:
+            return self.cleaned_data
+
+    def save(self):
+        data = self.data_to_save()
+        self.model(**data).save()
 
 
 class RegisterForm(Form):
 
-    required = ['username', 'password', 'password2', 'email']
+    model = User
+    fields = ['username', 'password', 'email', 'first_name', 'last_name', 'city']
+    required = ['username', 'password', 'password2', 'email', ]
+    max_length = {'username': 50, }
+    min_length = {'email': 5, }
     error_message = {
         'username': {
             'required': 'Wpisz poprawny nick!'
@@ -39,13 +101,12 @@ class RegisterForm(Form):
 
     def check_password(self):
         if self.cleaned_data['password'] != self.cleaned_data['password2']:
-            self.error['password'] = 'Hasła nie są takie same'
+            if not self.error.get('password'):
+                self.error['password'] = 'Hasła nie są takie same'
 
     def check_all(self):
         super(RegisterForm, self).check_all()
         self.check_password()
 
-
-
-
-
+    def save(self):
+        super(RegisterForm, self).save()
