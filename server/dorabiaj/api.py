f49513e74fb17084337affb677a3c15a89ed08c9
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-05-17 13:48:51 +0200

    Add api /myuser to get my user info

--- server/dorabiaj/api.py
+++ server/dorabiaj/api.py
@@ -182,6 +182,19 @@ def get_userinfo(username):
                  'opinions': user.opinions}
     return Response(json.dumps(user_info), status=200, content_type='application/json')
 
+@app.route('/myuser', methods=['GET'])
+@crossdomain(origin='http://localhost:5555')
+@login_required
+def get_myuserinfo(username):
+    try:
+        user = get_user()
+    except User.DoesNotExist:
+        error = {'error': 'Nie ma takiego użytkownika'}
+        return Response(json.dumps(error), status=200, content_type='application/json')
+    user_info = {'username': user.username, 'email': user.email, 'firstName': user.first_name,
+                 'lastName': user.last_name, 'city': user.city, 'admin': user.is_superuser, 'tokens': user.tokens,
+                 'opinions': user.opinions}
+    return Response(json.dumps(user_info), status=200, content_type='application/json')
 
 @app.route('/categories', methods=['GET'])
 @crossdomain(origin="http://localhost:5555")
